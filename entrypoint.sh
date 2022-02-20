518b28e99d4552079d260bf0e5e5f4563ff8a4be
Kamalakar Gadireddy <kgadireddy@bills.com>
2022-02-20 00:22:30 -0700

    ♻️ ssh added user specfic

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,20 @@ PUID=${PUID:-911}
 PGID=${PGID:-911}
 
 mkdir -p \
-    /config/{.ssh,ssh_host_keys,logs/openssh}
+  /config/{.ssh,ssh_host_keys,logs/openssh}
 
 mkdir -p /run/sshd
 
 if ! id "$1" &>/dev/null; then
-  groupmod -o -g "$PGID" "$USER_NAME"
-  usermod -o -u "$PUID" "$USER_NAME"
+  [ $(getent group developer) ] || groupadd -g $PGID -o developer
+  [ $(id -u $USER_NAME &>/dev/null) || useradd -m -u $PUID -g $PGID -o -s /bin/bash $USER_NAME 
+
+  mkdir -p \
+    /home/$USER_NAME/.ssh
 
   [[ -n "$PUBLIC_KEY" ]] && \
-      [[ ! $(grep "$PUBLIC_KEY" /config/.ssh/authorized_keys) ]] && \
-      echo "$PUBLIC_KEY" >> /config/.ssh/authorized_keys && \
+      [[ ! $(grep "$PUBLIC_KEY" /home/$USER_NAME/.ssh/authorized_keys) ]] && \
+      echo "$PUBLIC_KEY" >> //home/$USER_NAME/.ssh/authorized_keys && \
       echo "Public key from env variable added"
 fi
 
