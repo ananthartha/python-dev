4b438759fcd25d386639629286f095d9336bb505
Kamalakar Gadireddy <kgadireddy@bills.com>
2022-02-19 23:06:22 -0700

    ♻️ Create Dir Before AZ Keys get added

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,11 @@
 PUID=${PUID:-911}
 PGID=${PGID:-911}
 
+mkdir -p \
+    /config/{.ssh,ssh_host_keys,logs/openssh}
+
+mkdir -p /run/sshd
+
 if ! id "$1" &>/dev/null; then
   groupmod -o -g "$PGID" "$USER_NAME"
   usermod -o -u "$PUID" "$USER_NAME"
@@ -12,10 +17,5 @@ if ! id "$1" &>/dev/null; then
       echo "$PUBLIC_KEY" >> /config/.ssh/authorized_keys && \
       echo "Public key from env variable added"
 fi
-
-mkdir -p \
-    /config/{.ssh,ssh_host_keys,logs/openssh}
-
-mkdir -p /run/sshd
 
 /usr/sbin/sshd -D -e -p 22
