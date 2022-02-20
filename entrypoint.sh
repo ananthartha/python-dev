c8d7011c00fa4dbc7f57b67801e968936545d5dc
Kamalakar Gadireddy <kgadireddy@bills.com>
2022-02-19 22:53:54 -0700

    ♻️ Dir Creation

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,15 @@ if ! id "$1" &>/dev/null; then
   groupmod -o -g "$PGID" "$USER_NAME"
   usermod -o -u "$PUID" "$USER_NAME"
 
-  mkdir -p \
-      /config/{.ssh,ssh_host_keys,logs/openssh}
-
   [[ -n "$PUBLIC_KEY" ]] && \
       [[ ! $(grep "$PUBLIC_KEY" /config/.ssh/authorized_keys) ]] && \
       echo "$PUBLIC_KEY" >> /config/.ssh/authorized_keys && \
       echo "Public key from env variable added"
 fi
+
+mkdir -p \
+    /config/{.ssh,ssh_host_keys,logs/openssh}
+
+mkdir -p /run/sshd
 
 /usr/sbin/sshd -D -e -p 22
