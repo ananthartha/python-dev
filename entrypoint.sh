b4f90a260ffc308f7c0eff2c0585fc2740e627a0
Kamalakar Gadireddy <kgadireddy@bills.com>
2022-02-28 00:00:09 -0700

    ♻️ Updaed if cond

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,21 +8,19 @@ mkdir -p \
 
 mkdir -p /run/sshd
 
-if ! id "$1" &>/dev/null; then
-  [ $(getent group developer) ] || groupadd -g $PGID -o developer
+[ $(getent group developer) ] || groupadd -g $PGID -o developer
 
-  if [[ ! $(id -u $USER_NAME &>/dev/null) ]]; then
-    # Crete User
-    useradd -m -u $PUID -g $PGID -o -s /bin/bash $USER_NAME 
-    # Create Home Dir
-    mkdir -p /home/$USER_NAME/.ssh
+if [[ ! $(id -u $USER_NAME &>/dev/null) ]]; then
+  # Crete User
+  useradd -m -u $PUID -g $PGID -o -s /bin/bash $USER_NAME 
+  # Create Home Dir
+  mkdir -p /home/$USER_NAME/.ssh
 
-    [[ -n "$PUBLIC_KEY" ]] && \
-        [[ ! $(grep "$PUBLIC_KEY" /home/$USER_NAME/.ssh/authorized_keys) ]] && \
-        echo "$PUBLIC_KEY" >> /home/$USER_NAME/.ssh/authorized_keys && \
-        chown $USER_NAME:developer /home/$USER_NAME/.ssh/authorized_keys && \
-        echo "Public key from env variable added"
-  fi
+  [[ -n "$PUBLIC_KEY" ]] && \
+      [[ ! $(grep "$PUBLIC_KEY" /home/$USER_NAME/.ssh/authorized_keys) ]] && \
+      echo "$PUBLIC_KEY" >> /home/$USER_NAME/.ssh/authorized_keys && \
+      chown -R $USER_NAME:developer /home/$USER_NAME/ && \
+      echo "Public key from env variable added"
 fi
 
 /usr/sbin/sshd -D -e -p 22
